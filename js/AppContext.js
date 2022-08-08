31a83c92bd382f8a7fdfba6b0defc06cd6ec564f
Wojciech Pająk <arachnomorph@gmail.com>
2022-08-08 21:18:55 +0200

    Revert "infection pile now correctly sorts cards"
    This reverts commit 2a03177adab20abf3a74df87fe9e7cfd5498a079.

--- js/AppContext.js
+++ js/AppContext.js
@@ -17,7 +17,7 @@ function AppContextProvider({children}) {
             id: cards.length,
             description: val,
             discarded: true,
-            stack: 0
+            stack: 1
         }
         setCards(prev => prev.concat(card))
     }, [cards])
@@ -30,9 +30,9 @@ function AppContextProvider({children}) {
         if (isShuffled) {
             setCards(prev => prev.map(card => {
                 if (card.discarded) {
-                    return {...card, stack: card.stack, discarded: false}
+                    return {...card, stack: 1, discarded: false}
                 } else {
-                    return {...card, stack: card.stack + 1, discarded: false}
+                    return {...card, stack: 2, discarded: false}
                 }
             }))
         } else {
@@ -44,9 +44,11 @@ function AppContextProvider({children}) {
         const id = parseInt(e.target.id);
         setCards(prev => prev.map(card => {
             if (card.id !== id) {
+                console.log(card)
                 return {...card}
             }
-            return {...card, discarded: true, stack: 0}
+            console.log(card)
+            return {...card, discarded: true}
         }))
     }, [cards])
 
